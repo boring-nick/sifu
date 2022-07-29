d8466004341386b3922dbfe74ac470fd443ae175
boring_nick <boring-nick@users.noreply.github.com>
2022-07-30 00:00:32 +0300

    fix: hashing

--- src/hash_storage.rs
+++ src/hash_storage.rs
@@ -3,7 +3,7 @@ use blake3::Hash;
 use dashmap::DashMap;
 use std::{path::PathBuf, sync::Arc};
 use tokio::{
-    fs::File,
+    fs::{File, OpenOptions},
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
     sync::Mutex,
 };
@@ -14,7 +14,7 @@ pub const FILENAME: &str = "hashes.kv";
 #[derive(Clone)]
 pub struct HashStorage {
     pub map: Arc<DashMap<Hash, String>>, // Hash, filename
-    file: Arc<Mutex<File>>,
+    buf_reader: Arc<Mutex<BufReader<File>>>,
 }
 
 impl HashStorage {
@@ -22,17 +22,21 @@ impl HashStorage {
         if file_path.exists() {
             Self::load_from_file(file_path).await
         } else {
-            let file = File::create(&file_path).await?;
+            let reader = BufReader::new(File::create(&file_path).await?);
             Ok(Self {
                 map: Arc::new(DashMap::new()),
-                file: Arc::new(Mutex::new(file)),
+                buf_reader: Arc::new(Mutex::new(reader)),
             })
         }
     }
 
     pub async fn load_from_file(file_path: PathBuf) -> anyhow::Result<Self> {
-        let file = File::open(file_path).await?;
-        let mut reader = BufReader::new(file.try_clone().await?);
+        let file = OpenOptions::new()
+            .read(true)
+            .append(true)
+            .open(file_path)
+            .await?;
+        let mut reader = BufReader::new(file);
 
         let map = DashMap::new();
         let mut buf = Vec::with_capacity(64);
@@ -42,30 +46,30 @@ impl HashStorage {
             let hash = Hash::from_hex(&buf).context("could not parse hash")?;
             buf.clear();
 
-            while reader.read_until(b'\n', &mut buf).await? != 0 {
-                buf.remove(buf.len() - 1);
-                let file_name = String::from_utf8(buf)?;
+            reader.read_until(b'\n', &mut buf).await?;
 
-                map.insert(hash, file_name);
-                buf = Vec::with_capacity(64);
-            }
+            buf.remove(buf.len() - 1);
+            let file_name = String::from_utf8(buf)?;
+
+            map.insert(hash, file_name);
+            buf = Vec::with_capacity(64);
         }
 
         info!("Loaded {} hashes", map.len());
 
         Ok(Self {
             map: Arc::new(map),
-            file: Arc::new(Mutex::new(file)),
+            buf_reader: Arc::new(Mutex::new(reader)),
         })
     }
 
     pub async fn write_entry(&self, hash: &Hash, file_name: &str) -> anyhow::Result<()> {
-        let mut file = self.file.lock().await;
+        let mut reader = self.buf_reader.lock().await;
 
-        file.write_all(hash.to_hex().as_bytes()).await?;
-        file.write_all(b":").await?;
-        file.write_all(file_name.as_bytes()).await?;
-        file.write_all(b"\n").await?;
+        reader.write_all(hash.to_hex().as_bytes()).await?;
+        reader.write_all(b":").await?;
+        reader.write_all(file_name.as_bytes()).await?;
+        reader.write_all(b"\n").await?;
 
         debug!("Wrote hash entry for {file_name}");
 
